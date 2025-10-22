4741827f37c6fdccb4b8ab85499817d289f3d061
lenguyenvy1995 <42138419+lenguyenvy1995@users.noreply.github.com>
2025-10-22 10:54:11 +0700

    33

--- platform/themes/farmart/layouts/default.blade.php
+++ platform/themes/farmart/layouts/default.blade.php
@@ -1,6 +1,6 @@
 {!! Theme::partial('header') !!}
 
-<div id="main-content" style="background-image: url('https://traihomvanphuoc.b-cdn.net/./152e580b-576c-4aed-9e45-b2be9d3ddd49.webp')">
+<div id="main-content" >
     {!! Theme::partial('page-header', [
         'size' => Theme::get('containerSize', 'xl'),
         'withTitle' => Theme::get('withTitle', true),
