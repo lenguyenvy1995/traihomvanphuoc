0ade88fdaaa43cb0bb47d7e8dc6ef2045cf2d218
lenguyenvy1995 <42138419+lenguyenvy1995@users.noreply.github.com>
2025-12-02 16:13:43 +0700

    3526

--- platform/themes/farmart/views/obituary/index.blade.php
+++ platform/themes/farmart/views/obituary/index.blade.php
@@ -5,19 +5,23 @@
 
 <div class="container py-5">
     <div class="row">
-        @foreach($items as $item)
+        @foreach ($items as $item)
             <div class="col-md-4 mb-4">
                 <div class="card h-100">
 
                     {{-- Ảnh đại diện --}}
                     @if ($item->avatar)
-                        <img src="{{ RvMedia::getImageUrl($item->avatar) }}" class="card-img-top" alt="{{ $item->name }}">
+                        <a href="{{ $item->url }}">
+                            <img src="{{ RvMedia::getImageUrl($item->avatar) }}" class="card-img-top"
+                                alt="{{ $item->name }}">
+                        </a>
                     @endif
 
                     <div class="card-body" style="background-color: #000">
 
                         {{-- Tên --}}
-                        <h5 class="mb-2" style="text-transform: uppercase;color: #fbdb08;text-align: center;">{{ $item->name }}</h5>
+                        <h5 class="mb-2" style="text-transform: uppercase;color: #fbdb08;text-align: center;">
+                            {{ $item->name }}</h5>
 
                     </div>
 
